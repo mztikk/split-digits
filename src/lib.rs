693c53c999a182bc1fe04e1a5a34e7aa1b368597
mztikk <mztikk@outlook.de>
2022-04-30 14:04:58 +0200

    initial

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+pub struct SplitDigitIterator {
+    num: usize,
+}
+
+impl Iterator for SplitDigitIterator {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.num == 0 {
+            None
+        } else {
+            let digit = (self.num % 10) as u8;
+            self.num /= 10;
+            Some(digit)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::SplitDigitIterator;
+
+    #[test]
+    fn test_split_digits() {
+        let digits: Vec<u8> = SplitDigitIterator { num: 568764567 }.collect();
+        assert_eq!(digits, vec![7, 6, 5, 4, 6, 7, 8, 6, 5]);
+    }
+}
