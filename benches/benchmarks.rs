51b12f0ad580fb9afa4c0e2cc008fcbe52d10abf
mztikk <mztikk@outlook.de>
2024-11-05 02:53:21 +0100

    cleanup bench

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,72 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use split_digits::SplitDigitIterator;
 use std::collections::VecDeque;
-
-struct DigitAndPosition {
-    digit: u8,
-    position: u32,
-}
-
-fn get_most_significant_digit_and_position(d: usize) -> DigitAndPosition {
-    let mut d = d;
-    let mut position = 0;
-    let mut digit: u8 = 0;
-
-    while d > 0 {
-        digit = (d % 10) as u8;
-        d /= 10;
-        position += 1;
-    }
-
-    DigitAndPosition { digit, position }
-}
-
-struct SplitDigitIterator {
-    num: usize,
-    trailing_zeroes: u32,
-}
-
-impl SplitDigitIterator {
-    pub fn new(num: usize) -> Self {
-        Self {
-            num,
-            trailing_zeroes: 0,
-        }
-    }
-}
-
-impl Iterator for SplitDigitIterator {
-    type Item = u8;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.num != 0 {
-            let dap = get_most_significant_digit_and_position(self.num);
-            let digit = dap.digit;
-            let sub = 10_usize.pow(dap.position) * dap.digit as usize;
-            self.trailing_zeroes = dap.position;
-            self.num -= sub;
-            Some(digit)
-        } else {
-            if self.trailing_zeroes > 0 {
-                self.trailing_zeroes -= 1;
-                return Some(0);
-            }
-
-            None
-        }
-    }
-}
-
-impl DoubleEndedIterator for SplitDigitIterator {
-    fn next_back(&mut self) -> Option<Self::Item> {
-        if self.num != 0 {
-            let digit = (self.num % 10) as u8;
-            self.num /= 10;
-            Some(digit)
-        } else {
-            None
-        }
-    }
-}
 
 fn split_digits_vec_deque(d: usize) -> Vec<u8> {
     let mut digits = VecDeque::new();
@@ -98,13 +32,6 @@ fn split_digits_vec_push_reverse(d: usize) -> Vec<u8> {
         n /= 10;
     }
 
-    digits.reverse();
-
-    digits
-}
-
-fn split_digits_iterator_reverse(d: usize) -> Vec<u8> {
-    let mut digits: Vec<u8> = SplitDigitIterator::new(d).rev().collect();
     digits.reverse();
 
     digits
@@ -144,11 +71,6 @@ fn benchmark_split_digits(c: &mut Criterion) {
             BenchmarkId::new("split_digits_vec_push_reverse", s),
             d,
             |b, d| b.iter(|| split_digits_vec_push_reverse(black_box(*d))),
-        );
-        group.bench_with_input(
-            BenchmarkId::new("split_digits_iterator_reverse", s),
-            d,
-            |b, d| b.iter(|| split_digits_iterator_reverse(black_box(*d))),
         );
         group.bench_with_input(BenchmarkId::new("split_digits_iterator", s), d, |b, d| {
             b.iter(|| split_digits_iterator(black_box(*d)))
